5010b24de282e4aa76cf489760942dd0954263d5
Loïc Zeiss <loiczeiss@gmail.com>
2022-08-02 14:34:04 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,102 +1,43 @@
-document.getElementById("register").addEventListener("click", () => {
+function checkForm(form)
+  {
 
 
-})
+    if(form.pwd1.value != "" && form.pwd1.value == form.pwd2.value) {
+      if(form.pwd1.value.length < 6) {
+        alert("Error: Password must contain at least six characters!");
+        form.pwd1.focus();
+        return false;
+      }
+      if(form.pwd1.value == form.username.value) {
+        alert("Error: Password must be different from Username!");
+        form.pwd1.focus();
+        return false;
+      }
+      re = /[0-9]/;
+      if(!re.test(form.pwd1.value)) {
+        alert("Error: password must contain at least one number (0-9)!");
+        form.pwd1.focus();
+        return false;
+      }
+      re = /[a-z]/;
+      if(!re.test(form.pwd1.value)) {
+        alert("Error: password must contain at least one lowercase letter (a-z)!");
+        form.pwd1.focus();
+        return false;
+      }
+      re = /[A-Z]/;
+      if(!re.test(form.pwd1.value)) {
+        alert("Error: password must contain at least one uppercase letter (A-Z)!");
+        form.pwd1.focus();
+        return false;
+      }
+    } else {
+      alert("Error: Please check that you've entered and confirmed your password!");
+      form.pwd1.focus();
+      return false;
+    }
 
-/*!
- * avgrund 0.1
- * http://lab.hakim.se/avgrund
- * MIT licensed
- *
- * Copyright (C) 2018 Hakim El Hattab, http://hakim.se
- */
-var Avgrund = (function(){
-
-	var container = document.documentElement,
-		popup = document.querySelector( '.avgrund-popup-animate' ),
-		cover = document.querySelector( '.avgrund-cover' ),
-		currentState = null;
-
-	container.classList.add( 'avgrund-ready' );
-
-	// Deactivate on ESC
-	function onDocumentKeyUp( event ) {
-
-		if( event.keyCode === 27 ) {
-			deactivate();
-		}
-
-	}
-
-	// Deactivate on click outside
-	function onDocumentClick( event ) {
-
-		if( event.target === cover ) {
-			deactivate();
-		}
-
-	}
-
-	function activate( state ) {
-
-		document.addEventListener( 'keyup', onDocumentKeyUp, false );
-		document.addEventListener( 'click', onDocumentClick, false );
-		document.addEventListener( 'touchstart', onDocumentClick, false );
-
-		popup.classList.remove( currentState );
-		popup.classList.add( 'no-transition' );
-		if(state)
-			popup.classList.add( state );
-
-		setTimeout( function() {
-			popup.classList.remove( 'no-transition' );
-			container.classList.add( 'avgrund-active' );
-		}, 0 );
-
-		currentState = state;
-
-	}
-
-	function deactivate() {
-
-		document.removeEventListener( 'keyup', onDocumentKeyUp, false );
-		document.removeEventListener( 'click', onDocumentClick, false );
-		document.removeEventListener( 'touchstart', onDocumentClick, false );
-
-		container.classList.remove( 'avgrund-active' );
-		popup.classList.remove( 'avgrund-popup-animate' );
-
-	}
-
-	function disableBlur() {
-
-		document.documentElement.classList.add( 'no-blur' );
-
-	}
-
-	function show( selector ) {
-
-		popup = document.querySelector( selector );
-		popup.classList.add( 'avgrund-popup-animate' );
-		activate();
-		return this;
-
-	}
-
-	function hide() {
-
-		deactivate();
-
-	}
-
-	return {
-
-		activate: activate,
-		deactivate: deactivate,
-		disableBlur: disableBlur,
-		show: show,
-		hide: hide
-
-	}
-
-})();
+    alert("You entered a valid password: " + form.pwd1.value);
+    return true;
+  }
+  
