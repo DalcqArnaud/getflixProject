ca7ea5d09b07cbf3623f1ec7621432490a861586
Senait <77326649+seninet@users.noreply.github.com>
2022-08-03 10:06:43 +0200

    Senait search backend frontend (#12)
    * Add images - Update readme
    
    * ok im gettin there
    
    * Delete index.html
    
    * Delete script.js
    
    * Delete style.css
    
    * home-page first branch
    
    * upadate and hamberger menu added
    
    * api search bar backend and frontend added
    
    * search bar backend and frontend added
    
    Co-authored-by: ArnaudDalcq <aranaud.dalcq@gmail.com>
    Co-authored-by: LoicZeiss <loiczeiss@gmail.com>

---
+++ tvshow.js
@@ -0,0 +1,193 @@
+
+const API_KEY = 'api_key=f088ebb3ea3afd9640eb95267cc47330&language=en-US&page=1';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL = BASE_URL + '/discover/tv?sort_by=popularity.desc&' + API_KEY;
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
+const searchURL = BASE_URL + '/search/tv?' + API_KEY;
+
+const genres = []
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const tagsBox = document.getElementById('tags');
+
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
+const current = document.getElementById('current')
+
+let pageone = 1;
+let pagetwo = 2;
+let pagethree = 3;
+let totalPages = 50;
+
+let searchedGenre = []
+
+setGenre();
+
+function setGenre() {
+    tags.innerHTML = '';
+    genres.forEach(genre => {
+        const allDiv = document.createElement('div');
+        allDiv.classList.add('tag');
+        allDiv.id = genre.id;
+        allDiv.innerText = genre.name;
+        allDiv.addEventListener('click', () => {
+            if (selectedGenre.length == 0) {
+                selectedGenre.push(genre.id);
+            } else {
+                if (selectedGenre.includes(genre.id)) {
+                    selectedGenre.forEach((id, idx) => {
+                        if (id == genre.id) {
+                            selectedGenre.splice(idx, 1);
+                        }
+                    })
+                } else {
+                    selectedGenre.push(genre.id);
+                }
+            }
+            console.log(selectedGenre)
+            getTvShow(API_URL + '&with_genres=' + encodeURI(selectedGenre.join(',')))
+            highlightSelection()
+        })
+        tagsEl.append(allDiv);
+    })
+}
+
+function highlightSelection() {
+    const tags = document.querySelectorAll('.tag');
+    tags.forEach(tag => {
+        tag.classList.remove('highlight')
+    })
+    clearBtn()
+
+    if (selectedGenre.length != 0) {
+        selectedGenre.forEach(id => {
+            const hightlightedTag = document.getElementById(id);
+            hightlightedTag.classList.add('highlight');
+        })
+    }
+
+}
+
+function clearBtn() {
+    let clearBtn = document.getElementById('clear');
+    if (clearBtn) {
+        clearBtn.classList.add('highlight')
+    } else {
+        let clear = document.createElement('div');
+        clear.classList.add('tag', 'highlight');
+        clear.id = 'clear';
+        clear.innerText = 'Clear x';
+
+        clear.addEventListener('click', () => {
+            selectedGenre = [];
+            setGenre();
+            getTvShow(API_URL);
+        })
+        tagsEl.append(clear);
+    }
+}
+
+getTvShow(API_URL);
+
+function getTvShow(url) {
+    lastUrl = url;
+    fetch(url).then(res => res.json()).then(list => {
+        console.log(list.results)
+        if (list.results.length !== 0) {
+            showTV(list.results);
+            currentPage = list.page;
+            nextPage = currentPage + 1;
+            prevPage = currentPage - 1;
+            totalPages = list.total_pages;
+
+            current.innerText = currentPage;
+
+            if (currentPage <= 1) {
+                prev.classList.add('disabled');
+                next.classList.remove('disabled')
+            } else if (currentPage >= totalPages) {
+                prev.classList.remove('disabled');
+                next.classList.add('disabled')
+            } else {
+                prev.classList.remove('disabled');
+                next.classList.remove('disabled')
+            }
+
+            tagsEl.scrollIntoView({ behavior: 'smooth' })
+        } else {
+            main.innerHTML = `<h1 class="no-results">No Results Found</h1>`
+        }
+    })
+}
+
+function showTV(list) {
+    main.innerHTML = '';
+
+    list.forEach(movie => {
+        const { title, poster_path, id } = movie;
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML = `
+            <img src="${poster_path ? IMG_URL + poster_path : ""}" alt="${title}">
+
+                <h3>${title}</h3>
+            
+            </div>
+            
+                <button id="${id}">WATCH NOW</button> 
+        
+        `
+        main.appendChild(movieEl);
+
+        document.getElementById(id).addEventListener('click', () => {
+            console.log(id)
+            openNav(tv)
+        })
+    })
+}
+
+
+
+form.addEventListener('submit', (el) => {
+    el.preventDefault();
+    const searchTerm = search.value;
+    selectedGenre = [];
+    setGenre();
+    if (searchTerm) {
+        getTvShow(searchURL + '&query=' + searchTerm)
+    } else {
+        getTvShow(API_URL);
+    }
+
+})
+
+prev.addEventListener('click', () => {
+    if (prevPage > 0) {
+        pageCall(prevPage);
+    }
+})
+
+next.addEventListener('click', () => {
+    if (nextPage <= totalPages) {
+        rendPage(nextPage);
+    }
+})
+
+function rendPage(page) {
+    let urlSplit = lastUrl.split('?');
+    let queryParams = urlSplit[1].split('&');
+    let key = queryParams[queryParams.length - 1].split('=');
+    if (key[0] != 'page') {
+        let url = lastUrl + '&page=' + page
+        getTvShow(url);
+    } else {
+        key[1] = page.toString();
+        let a = key.join('=');
+        queryParams[queryParams.length - 1] = a;
+        let b = queryParams.join('&');
+        let url = urlSplit[0] + '?' + b
+        getTvShow(url);
+    }
+}
